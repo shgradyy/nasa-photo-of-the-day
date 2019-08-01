febaffa4a8a8603b85f9af49c345bbf4e407e2c9
Shantay <shantaygrady@gmail.com>
2019-08-01 18:15:48 -0400

    imports components, adds effects hook, fetches

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./App.css";
+import axios from "axios";
+import Viewer from "./components/viewer";
+import InfoPanel from "./components/infopanel";
+import PicToggler from ".components/buttons";
 
 function App() {
+  const [photo, setPhoto] = useState();
+  useEffect(() => {
+    axios
+    .get(https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY)
+
+
+  })
+  
+  
+  
+  
   return (
     <div className="App">
       <p>
